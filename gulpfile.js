06117cb4a51384a70216ee6d27ee3941084b0ce3
halfalpine <rocksandwoks@gmail.com>
2017-04-19 19:30:02 -0400

    Add browserSync to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,25 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var browserSync = require('browser-sync').create();
-
-gulp.task('hello', function() {
-  console.log('Hello Dirk');
-});
-
-gulp.task('sass', function(){
-  return gulp.src('app/sass/**/*.sass')
-    .pipe(sass())
-    .pipe(gulp.dest('app/css'))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
-});
+var browserSync = require('browser-sync');
 
 gulp.task('browserSync', function(){
-  browserSync.init({
+  browserSync({
     server: {
       baseDir: 'app'
     }
   })
-});
+})
+
+gulp.task('sass', function(){
+  return gulp.src('app/sass/index.sass')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+})
 
 gulp.task('watch', ['browserSync', 'sass'], function(){
-  gulp.watch('app/sass/**/*.sass', ['sass']);;
-  gulp.watch('app/*.html', browserSync.reload());
+  gulp.watch('app/sass/**/*.sass', ['sass']);
+  gulp.watch('app/*.html', browserSync.reload);
 });
